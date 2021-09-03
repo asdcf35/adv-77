30f2da13aa93c35caabc5c928b852f3c7ecc8835
asdcf35 <66645620+asdcf35@users.noreply.github.com>
2021-09-03 15:59:59 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var first_name = Ranbir;
+var last_name = Kapoor;
+var concat = first_name.concat(last_name);
+
+var a = 10;
+var b = 5;
+
+var add = a + b;
+var subtract = a-b;
+var multiply = a*b;
+var divide = a/b;
+
+console.log([add,subtract,multiply,divide]);
